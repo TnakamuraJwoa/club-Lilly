5a096c669f3e65945c5a317971cde36a1c2496e9
club Lilly <t.nakamura.jwoa@gmail.com>
2024-02-17 01:16:44 +0900

    naviの修正

--- js/common.js
+++ js/common.js
@@ -31,6 +31,20 @@ var navi_content_code = `
                <h3 class="">求人</h3>
              </li>
            </ul>
+           <!-- 電話するボタンを画面の下部に配置 -->
+           <div style="position: fixed; bottom: 200px;">
+            <div>
+            <!-- 電話ボタン -->
+            <a href="" class="btn btn--orange">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-plus-fill svg-icon" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877zM12.5 1a.5.5 0 0 1 .5.5V3h1.5a.5.5 0 0 1 0 1H13v1.5a.5.5 0 0 1-1 0V4h-1.5a.5.5 0 0 1 0-1H12V1.5a.5.5 0 0 1 .5-.5"/>
+              </svg>
+              <span>電話をかける</span>
+            </a>
+            <!-- Lineボタン -->
+            <a id="line-button" href="https://lin.ee/gZ4OQ8I"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="39px" border="0"></a>
+            </div>
+           </div>
          </div>
        </div>
      </div>
